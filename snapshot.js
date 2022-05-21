28a19f080018a937b626e3c31611f425670119bb
Christoph Knittel <ck@cca.io>
2022-05-21 09:10:24 +0200

    process.platform and os.platform() are the same

--- snapshot.js
+++ snapshot.js
@@ -3,14 +3,14 @@ var child = require("child_process");
 var fs = require("fs");
 var os = require("os");
 const path = require("path");
-var ext = os.platform();
+var platform = process.platform;
 var command = "python configure.py --bootstrap --verbose";
 function build() {
-  if (ext === "win32") {
+  if (platform === "win32") {
     // running on visual studio command line
     child.execSync(command, { cwd: __dirname });
   } else {
-    if (process.platform === "darwin") {
+    if (platform === "darwin") {
       process.env["CXXFLAGS"] = "-flto";
     }
     child.execSync(command, { stdio: [0, 1, 2], cwd: __dirname });
@@ -23,15 +23,15 @@ exports.build = build;
  * @type{string}
  */
 var dst;
-if (ext === 'darwin') {
-  dst = path.join(__dirname, "..", process.platform + process.arch, `ninja.exe`);
+if (platform === 'darwin') {
+  dst = path.join(__dirname, "..", platform + process.arch, `ninja.exe`);
 } else {
-  dst = path.join(__dirname, "..", ext, `ninja.exe`);
+  dst = path.join(__dirname, "..", platform, `ninja.exe`);
 }
 
 if (require.main === module) {
   build();
-  var src = path.join(__dirname, `ninja${ext === "win32" ? ".exe" : ""}`);
+  var src = path.join(__dirname, `ninja${platform === "win32" ? ".exe" : ""}`);
 
   fs.copyFileSync(src, dst);
 }
